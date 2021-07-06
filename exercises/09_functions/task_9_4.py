772277141d7fdd8f6481175098d41ea4a8a9538f
stuninslave <krasnenkovstas@gmail.com>
2021-07-06 07:45:53 -0500

    9.4 done

--- exercises/09_functions/task_9_4.py
+++ exercises/09_functions/task_9_4.py
@@ -64,3 +64,33 @@ def ignore_command(command, ignore):
         if word in command:
             ignore_status = True
     return ignore_status
+
+linetest='! Last configuration change at 13:11:59 UTC Thu Feb 25 2016'
+ignoring=ignore_command(linetest,ignore)
+print(ignore_command(linetest,ignore))
+
+def convert_config_to_dict(config_filename):
+    """
+    Фунция для обработки конфигурационного файла.
+    """
+    result={}
+    with open(config_filename,'r') as file:
+        for line in file:
+
+            if line.rstrip().startswith('!') or ignore_command(line.rstrip().strip(),ignore) or line.rstrip()=='':
+                continue                           #   Если строка с  "!" начинается - выходим из итерации цикла.
+            elif not line.rstrip().startswith(' '):        #   Если строка global level.
+
+                key=line.rstrip()                   # Записали ключ
+                result.setdefault(key)
+                result[key]=[]
+            elif ignore_command(line.strip(),ignore):
+                print(line.strip())
+                break
+            elif line.rstrip().startswith(' '):
+                result[key].append(line.rstrip().strip())
+
+    print(result)
+    return(result)
+
+convert_config_to_dict('config_sw1.txt')
