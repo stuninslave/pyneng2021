a140f211364e427fdd22d30c509a99e77cf76d64
stuninslave <krasnenkovstas@gmail.com>
2021-08-04 04:29:33 -0500

    task 12-3

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,19 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+alive=['1.1.1.1','2.2.2.2']
+dead=['192.168.0.7','192.168.2.2']
+
+from tabulate import tabulate
+
+def print_ip_table(alive_ip_list,dead_ip_list):
+    """
+    docstring
+    """
+    result = dict.fromkeys(['Reachable','Unreachable'])
+    result['Reachable'] = alive_ip_list
+    result['Unreachable'] = dead_ip_list
+    print(tabulate(result,headers="keys"))
+
+
+print_ip_table(alive,dead)
