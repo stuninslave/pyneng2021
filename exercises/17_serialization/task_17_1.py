3feb4f3ba16374e34535d1d05afaccc0ac11c210
stuninslave <krasnenkovstas@gmail.com>
2021-12-23 03:51:19 -0600

    Task 17-1 ready

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -31,12 +31,32 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 sw2_dhcp_snooping.txt, sw3_dhcp_snooping.txt.
 
 """
+import re
+import csv
 
 def write_dhcp_snooping_to_csv(filenames,output):
+    csv_list=[]
+    Columns_name=['switch','mac','ip','vlan','interface']
+    csv_list.append(Columns_name)
     for filename in filenames:
-        with open(filename,'r', encoding='utf-8') as f:
-            #  записть в переменную имя свича
+        with open(filename,'r') as f:
+            switch=filename.split('_')[0] #  записть в переменную имя свича
             for line in f:
                 # Распарсить строку конфига по regexp в лист.
+                regex=r'(?P<mac>\S\S:\S\S:\S\S:\S\S:\S\S:\S\S)\s+(?P<ip>\d+\.\d+\.\d+\.\d+)\s+\d+\s+\S+\s+(?P<vlan>\d+)\s+(?P<intf>\S+)'
+                match=re.search(regex,line)
+                if match:
+                    mac,ip,vlan,intf=match.groups()
+                    list1=[switch,mac,ip,vlan,intf]
+                    csv_list.append(list1)
                 # дописать распарсенную строку в список списков с указанием имени свича
+
     #экспорт списка списков в CSV
+    #print(csv_list)
+    with open(output,'w') as dest_file:
+        writer= csv.writer(dest_file, quoting=csv.QUOTE_NONNUMERIC)
+        for row in csv_list:
+            writer.writerow(row)
+
+write_dhcp_snooping_to_csv(['sw1_dhcp_snooping.txt','sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt'],'sw_csv.txt')
+
